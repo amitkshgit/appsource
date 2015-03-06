eb5a4eeeb72599cee21534cb42344be9a6c0c286
Amit Sharma <amitksh@yahoo.com>
2015-03-06 14:37:09 +0000

    Added php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+print gmdate("Y-m-d\TH:i:s\Z");
+$xforwardedfor  = getenv('HTTP_X_FORWARDED_FOR');
+
+$instanceid = file_get_contents("http://169.254.169.254/latest/meta-data/instance-id");
+$localipv4 = file_get_contents("http://169.254.169.254/latest/meta-data/local-ipv4");
+
+print "\t$instanceid\t$localipv4\t$xforwardedfor\n";
+
+?>
